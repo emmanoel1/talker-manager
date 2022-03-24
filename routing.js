02c8abebf2a84d4f6024cd67431bf9ba4fbe07d6
emmanoel <emmanoelporto@gmail.com>
2022-03-24 19:25:59 -0300

    fix: try fixing avaliator#3

--- routing.js
+++ routing.js
@@ -7,12 +7,12 @@ const talkerId = require('./endpointHandlers/talkerId');
 const talkerLogin = require('./endpointHandlers/login');
 
 const talkerAuth = require('./middlewares/talkerAuth');
-const talkerName = require('./middlewares/talkerName');
-const talkerAge = require('./middlewares/talkerAge');
-const talkerData = require('./middlewares/talkerData');
-const talkerSend = require('./middlewares/talkerSend');
+// const talkerName = require('./middlewares/talkerName');
+// const talkerAge = require('./middlewares/talkerAge');
+// const talkerData = require('./middlewares/talkerData');
+// const talkerSend = require('./middlewares/talkerSend');
 
-const putTalkerId = require('./endpointHandlers/putTalkerId');
+// const putTalkerId = require('./endpointHandlers/putTalkerId');
 const deleteTalkerId = require('./endpointHandlers/deleteTalkerId');
 
 router.get('/talker', talker);
@@ -21,9 +21,9 @@ router.get('/talker/:id', talkerId);
 
 router.post('/login', talkerLogin);
 
-router.post('/talker', talkerAuth, talkerName, talkerAge, talkerData, talkerSend);
+// router.post('/talker', talkerAuth, talkerName, talkerAge, talkerData, talkerSend);
 
-router.put('/talker/:id', talkerAuth, talkerName, talkerAge, talkerData, putTalkerId);
+// router.put('/talker/:id', talkerAuth, talkerName, talkerAge, talkerData, putTalkerId);
 
 router.delete('/talker/:id', talkerAuth, deleteTalkerId);
 
