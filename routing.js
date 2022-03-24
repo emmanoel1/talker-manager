95ee568c1e0c6305c294343ac6fa03f6cfd315c8
emmanoel <emmanoelporto@gmail.com>
2022-03-24 19:30:26 -0300

    fix: try fixing avaliator#4

--- routing.js
+++ routing.js
@@ -7,10 +7,10 @@ const talkerId = require('./endpointHandlers/talkerId');
 const talkerLogin = require('./endpointHandlers/login');
 
 const talkerAuth = require('./middlewares/talkerAuth');
-// const talkerName = require('./middlewares/talkerName');
-// const talkerAge = require('./middlewares/talkerAge');
-// const talkerData = require('./middlewares/talkerData');
-// const talkerSend = require('./middlewares/talkerSend');
+const talkerName = require('./middlewares/talkerName');
+const talkerAge = require('./middlewares/talkerAge');
+const talkerData = require('./middlewares/talkerData');
+const talkerSend = require('./middlewares/talkerSend');
 
 // const putTalkerId = require('./endpointHandlers/putTalkerId');
 const deleteTalkerId = require('./endpointHandlers/deleteTalkerId');
@@ -21,7 +21,7 @@ router.get('/talker/:id', talkerId);
 
 router.post('/login', talkerLogin);
 
-// router.post('/talker', talkerAuth, talkerName, talkerAge, talkerData, talkerSend);
+router.post('/talker', talkerAuth, talkerName, talkerAge, talkerData, talkerSend);
 
 // router.put('/talker/:id', talkerAuth, talkerName, talkerAge, talkerData, putTalkerId);
 
